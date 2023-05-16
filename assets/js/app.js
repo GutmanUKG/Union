215625b24c932f8b472f0fb339115a81d85959c7
Vladimir <vgutman95@mail.ru>
2023-05-16 17:30:03 +0600

    'add'

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+'use strict'
+document.addEventListener('DOMContentLoaded', ()=>{
+    let body = document.body;
+    function clearClass(items, clearClass){
+        for(let i = 0; i < items.length; i++){
+            items[i].classList.remove(clearClass)
+        }
+    }
+})
---
+++ public/js/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+  var body = document.body;
+  function clearClass(items, clearClass) {
+    for (var i = 0; i < items.length; i++) {
+      items[i].classList.remove(clearClass);
+    }
+  }
+});
+//# sourceMappingURL=app.js.map
